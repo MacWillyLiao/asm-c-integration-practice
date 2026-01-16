8a3f49405f86a5cf9290e3bbc01915fd6650ce1f
MacWillyLiao <116270282+MacWillyLiao@users.noreply.github.com>
2026-01-16 09:12:05 +0800

    Update id.c

--- data/id.c
+++ data/id.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-//Dummy Function. Please refer to the specification of Project 1.
 void id( )
 {
 	printf( "*****Print Team Member ID and ID Summation*****\n" );
@@ -9,4 +8,5 @@ void id( )
 	printf( "10027003\n" );
 	printf( "ID Summation = 30081006\n" );
 	printf( "*****End Print*****\n" );
+
 }

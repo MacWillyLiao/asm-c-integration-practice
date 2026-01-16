b94a0cd0e3a31d3f796f6baed5349cf2c34aea55
MacWillyLiao <116270282+MacWillyLiao@users.noreply.github.com>
2026-01-16 09:21:53 +0800

    Fix indentation in assembly source file

--- src/name.s
+++ src/name.s
@@ -1,4 +1,4 @@
-	.data
+		.data
 msg1:   .asciz  "*****Print Name*****\n"
 msg2:   .asciz  "*****End Print*****\n"
 team:   .asciz  "Team 05\n"
@@ -8,23 +8,23 @@ name3:  .asciz  "Liao, Yi Wei\n"
 
         .text
         .global name	    @ Function name
-	.global name1	
-	.global name2
-	.global name3
+		.global name1	
+		.global name2
+		.global name3
 
 name:   stmfd   sp!, {lr}
 @-----------------------------------------------------------------------
-        ldr     r0, =msg1	@ r0 = "*****Print Name*****\n"
+        ldr     r0, =msg1		@ r0 = "*****Print Name*****\n"
         bl      printf
-        ldr     r0, =team	@ r0 = "Team 05\n"
+        ldr     r0, =team		@ r0 = "Team 05\n"
         bl      printf
-        ldr     r0, =name1	@ r0 = "Kao, Hung Chun\n"
+        ldr     r0, =name1		@ r0 = "Kao, Hung Chun\n"
         bl      printf
-        ldr     r0, =name2	@ r0 = "Liao, Yi Wei\n"
+        ldr     r0, =name2		@ r0 = "Liao, Yi Wei\n"
         bl      printf
-        ldr     r0, =name3	@ r0 = "Liao, Yi Wei\n"
+        ldr     r0, =name3		@ r0 = "Liao, Yi Wei\n"
         bl      printf
-        ldr     r0, =msg2	@ r0 = "*****End Print*****\n"
+        ldr     r0, =msg2		@ r0 = "*****End Print*****\n"
         bl      printf
 @-----------------------------------------------------------------------
         ldmfd   sp!, {lr}
